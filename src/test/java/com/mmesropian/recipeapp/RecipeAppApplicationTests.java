c1281e5d8b848dbaa9c20e40130e372ea1ba580b
Michael <mmesropian@gmail.com>
2023-02-24 16:44:14 +0200

    spring initial

---
+++ src/test/java/com/mmesropian/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmesropian.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
