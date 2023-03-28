fe86b0b7ac5a4fe72c789ad48c033ef33c95f3a2
Michael <mmesropian@gmail.com>
2023-03-28 20:01:32 +0300

    added Lombok annotation

--- src/main/java/com/mmesropian/recipeapp/controllers/IndexController.java
+++ src/main/java/com/mmesropian/recipeapp/controllers/IndexController.java
@@ -1,16 +1,13 @@
 package com.mmesropian.recipeapp.controllers;
 
-import com.mmesropian.recipeapp.domain.Category;
-import com.mmesropian.recipeapp.domain.UnitOfMeasure;
-import com.mmesropian.recipeapp.repositories.CategoryRepositories;
-import com.mmesropian.recipeapp.repositories.UnitOfMeasureRepositories;
 import com.mmesropian.recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -22,6 +19,7 @@ public class IndexController {
 
     @RequestMapping({"","/","/index"})
     public String getIndexPage(Model model) {
+        log.debug("Getting Index Page");
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
